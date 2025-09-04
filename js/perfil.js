4ed4f5d0c4a68c9e7c9df87319bbbe052c16cfba
Kevin Henrique Davi <kevin1080p.mdm@gmail.com>
2025-09-04 16:56:18 -0300

    Adicionado a página de Perfil de Usuário

---
+++ js/perfil.js
@@ -0,0 +1,6 @@
+// Ao carregar a página exacuta as funções de buscar os dados
+document.addEventListener("DOMContentLoaded", async () => {
+    setTimeout(() => {
+        toggleLoading();
+    }, 1000);
+});
