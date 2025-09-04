f2dd8b83202272e913fbb55a1a44732c3d407dd5
Kevin Henrique Davi <kevin1080p.mdm@gmail.com>
2025-09-04 16:09:11 -0300

    Função de Scrollar Ao passar o Mouse por cima

--- js/index.js
+++ js/index.js
@@ -12,3 +12,41 @@ document.addEventListener("DOMContentLoaded", async () => {
                     </a>`;
     }
 });
+
+// Trending Movies Scroll
+const containerTrendingMovies = document.getElementById("trendingMovies");
+
+let scrollIntervalTrendingMovies; //Controlador para o intervalo de scroll
+let scrollDirectionTrendingMovies = 0; // Direção do scroll (0 = parado, 1 = direita, -1 = esquerda)
+
+containerTrendingMovies.addEventListener("mousemove", (e) => {
+    const boundingRect = containerTrendingMovies.getBoundingClientRect();
+    const mouseX = e.clientX;
+
+    const threshold = 200; //Distancia das bordas para ativar o scroll
+
+    if(mouseX < boundingRect.left + threshold){
+        scrollDirectionTrendingMovies = -1 //Scroll para Esquerda
+        containerTrendingMovies.style.cursor = "url('/img/arrow-rigth.png'), auto"; // cursor para a esquerda
+    }else if (mouseX > boundingRect.right - threshold) {
+        scrollDirectionTrendingMovies = 1; // Scroll para a direita
+        containerTrendingMovies.style.cursor = "url('/img/arrow-right.png'), auto"; //Cursor para a direita
+    } else {
+        scrollDirectionTrendingMovies = 0; //parar o Scroll
+        containerTrendingMovies.style.cursor = "pointer"; // Cursor padrão
+    }
+});
+
+containerTrendingMovies.addEventListener("mouseleave", () => {
+    scrollDirectionTrendingMovies = 0; //Parar o cursos quando o mouse sai do elemento
+    containerTrendingMovies.style.cursor = "default"; //Resetar o Cursor
+});
+
+//Função para scroll continuo 
+function autoScrollTrendingMovies() {
+    if(scrollDirectionTrendingMovies !== 0 ) {
+        containerTrendingMovies.scrollLeft += scrollDirectionTrendingMovies *6; //Ajuste a velocidade (5 = Rapido)
+    }
+}
+
+scrollDirectionTrendingMovies = setInterval(autoScrollTrendingMovies, 16); // ~60 FPS
